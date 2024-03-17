41604a48b58fec1280cd03285f7eb84b057a67a0
robertonakato <132300963+robertonakato@users.noreply.github.com>
2024-03-17 16:28:12 -0300

    Add files via upload

---
+++ assebles/js/etapas.js
@@ -0,0 +1,48 @@
+let etapas =[
+    {
+    titulo : "Governador",
+    numeros : 5,
+    candidatos : [
+        {
+        codico:'96762',
+        nome: "Ferdinand",
+        Partido: "Presas&preds",
+        foto:[{
+            URL:"./assebles/img/Candidatos/FERDINAND.png",legenda:"Governador"
+        }]
+
+    },
+    {
+        codico:'22881',
+        nome: "Daham Karmaico",
+        Partido: "PFG",
+        foto:[{
+            URL:"./assebles/img/Candidatos/governador.png",legenda:"Governador"
+        }]
+    }
+
+]
+},
+{
+    titulo : "Prefeito",
+    numeros : 2,
+    candidatos : [{
+        codico:'69',
+        nome: "Ray Lizard",
+        Vice:"Adrian parada",
+        Partido: "Presas&preds",
+        foto:[{URL:"./assebles/img/Candidatos/RAY.png",legenda:"Prefeito"},{URL:"./assebles/img/Candidatos/adrian.png",legenda:"Vice-Prefeito", small:true}
+        ]
+    },
+    {
+        codico:'14',
+        nome: "Eduard Timoend",
+        Vice: "Audrey Alinkar",
+        Partido: "YHJ",
+        foto:[{URL:"./assebles/img/Candidatos/EDUARD.png",legenda:"Prefeito"},{URL:"./assebles/img/Candidatos/AUDREY.jpeg",legenda:"Vice-Prefeito", small:true}
+        ]
+    }
+
+]
+}
+];
---
+++ assebles/js/script.js
@@ -0,0 +1,128 @@
+let seuvotopara = document.querySelector(".d1_esquerda_1 span")
+let cargo = document.querySelector(".d1_esquerda_2 span")
+let descricao = document.querySelector(".d1_esquerda_4")
+let aviso = document.querySelector(".d2")
+let lateral = document.querySelector(".d1_direita")
+let numeros = document.querySelector(".d1_esquerda_3")
+
+let etapaatual = 0;
+let numerocandidato = ''
+ let votobranco = false
+
+function comecarEtapa(){
+    let etapa = etapas[etapaatual]
+    let numeroHTML = "";
+    numerocandidato = ''
+    votobranco = false
+    votos =[];
+
+    for( let i=0; i< etapa.numeros; i++){
+        if( i===0){
+            numeroHTML += '<div class="numero pisca"></div>'
+        }
+        else{numeroHTML += '<div class="numero"></div>'}
+    }
+    seuvotopara.style.display = "none"
+    cargo.innerHTML = etapa.titulo
+    descricao.innerHTML = ""
+    aviso.style.display = "none"
+    lateral.innerHTML = ""
+    numeros.innerHTML = numeroHTML;
+}
+
+
+function atualizarInterface(){
+    let etapa = etapas[etapaatual]
+    let candidato = etapa.candidatos.filter((item)=>{
+        if(item.codico === numerocandidato ){
+            return true 
+        }
+        else {
+             return false
+
+        }
+    })
+    if(candidato.length > 0){
+        candidato = candidato[0];
+        seuvotopara.style.display = "block"
+        if(candidato.Vice !== undefined){
+            descricao.innerHTML = `Nome:${candidato.nome} </br> Vice: ${candidato.Vice} </br> Partido: ${candidato.Partido} </br> Código: ${candidato.codico}`
+        }
+        else{
+            descricao.innerHTML = `Nome:${candidato.nome} </br> Partido: ${candidato.Partido} </br> Código: ${candidato.codico}`
+        }
+
+        let fotoshtml =''
+        
+        for(let i in candidato.foto){
+            fotoshtml +=`<div class="d1_direita"> <div class="d1_direita_img"><img src=${candidato.foto[i].URL} alt=""> ${candidato.foto[i].legenda}</div>`
+        }
+        lateral.innerHTML = fotoshtml;
+    } else{
+        seuvotopara.style.display = "block"
+        aviso.style.display = "block"
+        descricao.innerHTML = '<div class ="aviso_grande pisca">VOTO NULO</div> '
+
+
+
+    }
+
+}
+function clicou(n){
+    let codigos = document.querySelector(".numero.pisca")
+    if( codigos !== null){
+        codigos.innerHTML = n;
+        numerocandidato += n
+        codigos.classList.remove("pisca")
+        if(codigos.nextElementSibling !==null){
+            codigos.nextElementSibling.classList.add("pisca")
+        }
+        else{
+            atualizarInterface()
+        }
+    }
+
+}
+function branco(){
+    if(numerocandidato === ''){
+    votobranco = true;
+    seuvotopara.style.display = "block";
+    aviso.style.display = "block";
+    numeros.innerHTML = '';
+    descricao.innerHTML = '<div class ="aviso_grande pisca">VOTO EM BRANCO</div> '
+
+    }
+
+}
+function corrige(){
+    comecarEtapa()
+}
+
+function confirmar(){
+    let etapa = etapas[etapaatual]
+    let confirmandovoto = false
+    if(votobranco === true){
+    confirmandovoto = true
+         votos.push({ 
+           etapa: etapas[etapaatual].titulo,
+            voto:'branco'
+        })
+    }
+    else if (numerocandidato.length === etapa.numeros ){
+        confirmandovoto = true
+        votos.push({ 
+            etapa: etapas[etapaatual].titulo,
+             voto:numerocandidato
+         })
+    }
+    if (confirmandovoto) {
+        etapaatual++
+        if(etapas[etapaatual] !==undefined){
+            comecarEtapa()
+        }
+        else{
+            document.querySelector(".tela").innerHTML = '<div class ="aviso_gigante pisca">FIM</div>'
+        }
+    }
+}
+comecarEtapa();
